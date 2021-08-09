1fdd72ab7101cf10dca450cda88edde13d7091d2
Oever González <software@notengobattery.com>
2021-08-09 00:54:05 -0600

    Add CSV parsing to the internal representation
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/jobs/extract_csv_job.rb
+++ app/jobs/extract_csv_job.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class ExtractCsvJob < ApplicationJob
   queue_as :default
 
@@ -13,7 +15,31 @@ class ExtractCsvJob < ApplicationJob
     resource.save!
   end
 
-  def perform(_params = {})
-    sleep 5.seconds
+  def perform(params = {})
+    create_ir(params[:resource])
   end
+
+  private
+    # No time to optimize this
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def create_ir(resource)
+      # Try to make the save an atomic operation by holding the mid process in memory and writing until it's done
+      new_ir = resource.ir || {}
+      resource.contacts_file.open do |csv|
+        parsed = CSV.read(csv.path, headers: true, skip_blanks: true)
+        new_ir[:headers] = parsed.headers.index_with { |_item| nil }
+        records = new_ir[:records] || []
+        parsed.each do |record|
+          record_hash = {}
+          record.each do |key, val|
+            record_hash[key] = val
+          end
+          records.push({ data: record_hash })
+        end
+        new_ir[:records] = records
+      end
+      resource.ir = new_ir
+      resource.save!
+    end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
