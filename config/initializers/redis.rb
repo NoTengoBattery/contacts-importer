d0205648505f37977449a91dcdc7ed776f4e25ff
Oever González <software@notengobattery.com>
2021-08-09 04:40:23 -0600

    Fix Redis for Heroku
    Signed-off-by: Oever González <software@notengobattery.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-Resque.redis = Redis.new(url: ENV["REDIS_DATABASE_URL"]) if Rails.env.production?
+# rubocop:disable Style/GlobalVars
+$redis = Resque.redis = Redis.new(url: ENV["REDIS_DATABASE_URL"]) if Rails.env.production?
+# rubocop:enable Style/GlobalVars
