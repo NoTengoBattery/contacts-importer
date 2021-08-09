4b71e29bbd429cec9867c6d4398abe46c644c228
Oever González <software@notengobattery.com>
2021-08-08 21:29:02 -0600

    Make the contact list index the root path
    Signed-off-by: Oever González <software@notengobattery.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 require "domain_routing"
 
 Rails.application.routes.draw do
-  get "contact_lists/index"
   devise_for :users
   resources :users, only: [:show]
 
+  resources :contact_lists, only: [:index]
+
   patch "/locale/:locale", action: :site_locale, controller: :locales, as: :site_locale
   put "/locale", action: :default_locale, controller: :locales, as: :default_locale
+
+  root "contact_lists#index"
 end
