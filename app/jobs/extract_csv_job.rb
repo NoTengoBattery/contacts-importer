dda68e9909ded9b4e7ab97bd346cd768f55ecac4
Oever González <software@notengobattery.com>
2021-08-08 23:43:12 -0600

    Add support for fake job and showing the status
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/jobs/extract_csv_job.rb
+++ app/jobs/extract_csv_job.rb
@@ -1,5 +1,19 @@
 class ExtractCsvJob < ApplicationJob
   queue_as :default
 
-  def perform(params = {}); end
+  before_perform do |job|
+    resource = job.arguments[0][:resource]
+    resource.status = "processing"
+    resource.save!
+  end
+
+  after_perform do |job|
+    resource = job.arguments[0][:resource]
+    resource.status = "finished"
+    resource.save!
+  end
+
+  def perform(_params = {})
+    sleep 5.seconds
+  end
 end
