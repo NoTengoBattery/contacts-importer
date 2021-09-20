de91263b07dc83be6e33e6186dd827707f19b35f
Oever González <software@notengobattery.com>
2021-09-20 00:56:01 -0600

    Add some validations for the contacts model
    Signed-off-by: Oever González <software@notengobattery.com>

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,7 +4,21 @@ require "shared_rutines"
 # rubocop:disable RSpec/EmptyExampleGroup
 RSpec.describe Contact, type: :model do
   context "with validations" do
-    subject { FactoryBot.build(:contact) }
+    subject(:contact) { FactoryBot.build(:contact) }
+
+    it { is_expected.to validate_presence_of(:address) }
+
+    # rubocop:disable RSpec/MultipleExpectations
+    it { is_expected.to validate_presence_of(:birth_date) }
+
+    it "is expected to validate that :birth_date is ISO8601" do
+      contact.birth_date = "01 Jan 1970"
+      expect(contact).to be_invalid
+
+      contact.birth_date = "19700101"
+      expect(contact).to be_valid
+    end
+    # rubocop:enable RSpec/MultipleExpectations
   end
 end
 # rubocop:enable RSpec/EmptyExampleGroup
