51e323fd4e543d5d936bc468aaf7b41e5f0edb9c
Oever González <software@notengobattery.com>
2021-09-20 21:49:47 -0600

    Segregate testing of validator class

--- app/models/concerns/iso8601_validator.rb
+++ app/models/concerns/iso8601_validator.rb
@@ -1,5 +1,6 @@
 class Iso8601Validator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    raise Date::Error unless value.is_a? String
     Date.iso8601(value)
   rescue Date::Error
     record.errors.add(attribute, I18n.t("errors.messages.iso8601"))
