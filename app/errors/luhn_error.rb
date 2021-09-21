dab80dbe3b443f147c6694d9dd3deded5e74c50c
Oever González <software@notengobattery.com>
2021-09-20 23:39:23 -0600

    Add Luhn edge cases
    Signed-off-by: Oever González <software@notengobattery.com>

---
+++ app/errors/luhn_error.rb
@@ -0,0 +1,1 @@
+class LuhnError < StandardError; end
--- spec/models/validators/credit_card_validator_spec.rb
+++ spec/models/validators/credit_card_validator_spec.rb
@@ -14,17 +14,25 @@ RSpec.describe CreditCardValidator do
     end).new
   end
 
+  # Design consideration:
+  # While I may test the valid credit card and add/subtract a randon number and expect it to fail, it may actually not
+  # work as expected as the negative test may randomly fail. Adding a constant will not allow the tests to cover edge
+  # cases.
+  # Crafting the invalid numbers with ones that are guaranteed to be invalid but modified in diverse ways is a better
+  # choice.
   context "with invalid credit card" do
     [
       nil, "", "invalid", 1234, :invalid,
       "378282246310004", "378285246310005",
-      "5555555555554443", "5555555515554444",
-      "5155555555554447", "5155555515554448",
+      "5555555555554445", "5555551555554444",
+      "5155555555554440", "5155555155554448",
       "5355555555554445", "5355555515554446",
-      "41111111111113", "42111111111114",
-      "4111111111111110", "4211111111111111"
+      "41111111111115", "42111111111114",
+      "4111111111111110", "4211111111111111",
+      "411111111111116", "4111111111119",
+      "53555555555544452", "535555555544458"
     ].each do |input|
-      describe "#{input.class} credit card with value #{input.inspect}" do
+      context "with type:#{input.class} and value #{input.inspect}" do
         it "fails validation" do
           rtest.dummy = input
           expect(rtest).to be_invalid
@@ -39,7 +47,7 @@ RSpec.describe CreditCardValidator do
       "5555555555554444", "5155555555554448", "5355555555554446",
       "41111111111114", "4111111111111111"
     ].each do |input|
-      describe "#{input.class} credit card with value #{input.inspect}" do
+      context "with type:#{input.class} and value #{input.inspect}" do
         it "passes validation" do
           rtest.dummy = input
           expect(rtest).to be_valid
