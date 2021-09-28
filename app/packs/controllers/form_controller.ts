3483695afab4a54f521fa85dbea44d572afd7be9
Oever González <software@notengobattery.com>
2021-09-28 08:52:45 -0600

    Upgrade Stimulus
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/packs/controllers/form_controller.ts
+++ app/packs/controllers/form_controller.ts
@@ -1,10 +1,14 @@
-import { Controller } from 'stimulus'
+import { Controller } from '@hotwired/stimulus'
 import { enableElement } from '@rails/ujs'
 
 export default class extends Controller {
+  readonly resetTarget!: Element
+  readonly resetTargets!: Element[]
+  readonly hasResetTarget!: boolean
+
   static targets = ['reset'];
 
-  reset () {
+  reset (): void {
     this.resetTargets.forEach((element) => {
       enableElement(element)
     })
---
+++ app/packs/controllers/index.ts
@@ -0,0 +1,13 @@
+// Load all the controllers within this directory and all subdirectories.
+// Controller files must be named *_controller.js.
+
+import { Application } from '@hotwired/stimulus'
+import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
+
+declare global {
+    interface Window { Stimulus: any; }
+}
+
+window.Stimulus = Application.start()
+const context = require.context('controllers', true, /_controller\.[jt]s$/)
+window.Stimulus.load(definitionsFromContext(context))
