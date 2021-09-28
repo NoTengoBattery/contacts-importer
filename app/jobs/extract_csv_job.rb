32007331d59e45db81efcb95a1f6b67dc32bc36a
Oever González <software@notengobattery.com>
2021-09-28 01:07:32 -0600

    Fix the CSV job to satisfy the test suite
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/jobs/extract_csv_job.rb
+++ app/jobs/extract_csv_job.rb
@@ -9,48 +9,47 @@ class ExtractCsvJob < ApplicationJob
     resource.save!
   end
 
-  after_perform do |job|
-    resource = job.arguments[0][:resource]
-    resource.status = :finished unless resource.needs_mappings? || resource.failed?
-    # resource.save!
-  end
-
   def perform(params = {})
     resource = params[:resource]
     create_ir(resource) if resource.processing?
     store_contacts(resource) if resource.mapped?
   end
 
+  after_perform do |job|
+    resource = job.arguments[0][:resource]
+    resource.status = :finished unless resource.needs_mappings? || resource.failed?
+    resource.save!
+  end
+
   private
 
-  # No time to optimize this
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def create_ir(resource)
-    new_ir = resource.ir || {}
+    resource.ir = {"records" => []}
     resource.contacts_file.open do |csv|
       parsed = CSV.read(csv.path, headers: true, skip_blanks: true)
-      new_ir[:headers] = parsed.headers.index_with { |_item| nil }
-      records = new_ir[:records] || []
+      resource.ir["headers"] = parsed.headers.index_with(nil)
       parsed.each do |record|
-        records.push(record.each_with_object({}) do |(key, val), acc|
-          acc[key] = val
-        end)
+        (resource.ir["records"]).push(
+          record.each_with_object({}) { |(key, val), acc| acc[key] = val }
+        )
       end
-      new_ir[:records] = records
     end
-    resource.ir = new_ir
     resource.status = :needs_mappings
     resource.save!
   end
 
   def store_contacts(resource)
+    failure = false
     resource.ir["records"].each do |record|
       params = record.transform_keys do |key|
         resource.ir["headers"][key]
       end
+      params.compact!
+      params.delete(nil)
       contact = resource.contacts.build({details: params})
-      contact.save
+      failure |= contact.save
     end
+    resource.reload
+    resource.status = "failed" unless failure
   end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
