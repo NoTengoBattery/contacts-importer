9ca5b1f470cc9d026a039ccea0411fbff8d71630
Oever González <software@notengobattery.com>
2021-09-29 12:14:02 -0600

    Rename dom_uuid to dom_uid
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
     button_to name, site_locale_path(id), params
   end
 
-  def dom_uuid(*thing)
+  def dom_uid(*thing)
     Rails.cache.fetch(CacheKey.gen!(thing)) do
       "I#{Base64.urlsafe_encode64(Digest::MD5.digest(Marshal.dump(thing)), padding: false)}"
     end
